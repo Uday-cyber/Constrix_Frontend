e4ee6a9e642d41750d46546a0781379f5c9ffa8e
Your Name <you@example.com>
2026-03-15 13:01:14 +0530

    add jsxInject option to esbuild in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
   plugins: [tailwindcss()],
 })
